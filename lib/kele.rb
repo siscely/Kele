745ce56a57df0c0cdad981322dddfa726df8014b
siscely <siscely.george@gmail.com>
2017-12-18 23:04:56 +0000

    commit changes

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,4 +1,6 @@
 require 'httparty'
+require 'roadmap'
+include RoadMap
 
 
 class Kele
@@ -8,5 +10,15 @@ class Kele
          @base_uri = "https://www.bloc.io/api/v1"        
          response = self.class.post("#{@base_uri}/sessions", body: {"email": email, "password": password})
          @auth_token = response["auth_token"]
+    end
+    
+    def get_me
+       response = self.class.get("#{@base_uri}/users/me", headers: { "authorization" => @auth_token })
+       JSON.parse(response.body)
+    end
+    
+    def get_mentor_availability(mentor_id)
+        response = self.class.get("#{@base_uri}/mentors/id/student_availability", headers: { "authorization" => @auth_token })
+        JSON.parse(response.body)
     end
 end
---
+++ lib/roadmap.rb
@@ -0,0 +1,12 @@
+module RoadMap
+    
+    def get_roadmap
+        response = self.class.get("#{@base_uri}/roadmaps/37", headers: { "authorization" => @auth_token })
+        JSON.parse(response.body)
+    end
+    
+    def get_checkpoint
+        response = self.class.get("#{@base_uri}/checkpoints/2260", headers: { "authorization" => @auth_token })
+        JSON.parse(response.body)
+    end
+end
